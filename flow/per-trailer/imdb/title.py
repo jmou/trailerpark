2abce91f928a304fd0dd60858d1342708270cc7b
Alice <alice@example.com>
2021-03-26 21:25:46 +0000

    fix titles

--- flow/per-trailer/imdb/title.py
+++ flow/per-trailer/imdb/title.py
@@ -2,4 +2,4 @@ import json
 import urllib.parse
 
 data = json.load(open('@OUT(imdb/scrape.sh,out/json)'))
-print(urllib.parse.quote_plus(data['name']))
+print(urllib.parse.quote_plus(data['trailer']['name']))
